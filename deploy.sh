7513cede17879e486ff780e63a5ea59e3438c750
许志清 <xuzhiqing2014@163.com>
2018-07-08 23:12:52 +0800

    修改脚本

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 echo "############ start deploy ############"
-sshpass -p w123456w ssh root@118.24.99.216 "/usr/deploy.sh"
+ssh root@118.24.99.216 "/usr/deploy.sh"
 echo "############ deploy complete ############"
